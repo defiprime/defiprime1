93fe930948069f03ec87aa3b5950730cd087024b
sneg55 <nikita@sawinyh.ru>
2019-09-14 12:26:44 -0700

    Update insert_date.sh

--- insert_date.sh
+++ insert_date.sh
@@ -1,10 +1,10 @@
-# Run this script inside of directory with .md files. 
+# Run this script inside of directory with .md files.
 # Datetime of added date will ve written at second line of .md file. If it is already exist - will just overwrited
 
 for file in $(git ls-files "*.md")
 do
     HASH=$(git rev-list HEAD "$file" | tail -n 1)
-    DATE=$(git show -s --format="%ci" $HASH --)
-    sed -i "2s/git-date.*$/git-date: $DATE/; t; 2s/^/git-date: $DATE\r\n/" $file
+    DATE=$( git log --first-parent master --diff-filter=A --follow --format=%aI -- $file | tail -1 --)
+    gsed -i "2s/git-date.*$/git-date: $DATE/; t; 2s/^/git-date: $DATE\r\n/" $file
     printf "%-35s %s\n  %s\n" "$file" $HASH: "$DATE"
 done
